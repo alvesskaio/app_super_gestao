b3bcc675cc75b30af977a6eee1701d8c434281c2
alvesskaio <kaio.alves19@gmail.com>
2022-06-13 15:10:29 -0300

    Estudo feito sobre agrupamento de Routes, nomeação de Routes e redirecionamento de Routes

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,7 +1,9 @@
 <h3>Principal (views)</h3>
 
 <ul>
-    <li><a href="/">Principal</a></li>
-    <li><a href="/sobre-nos">Sobre-nós</a></li>
+    
+    <li><a href="{{ route('site.index') }}">Principal</a></li>
+
+    <li><a href="{{ route('site.sobreNos') }}">Sobre-nós</a></li>
     <li><a href="/contato">Contato</a></li>
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,34 @@ use Illuminate\Support\Facades\Route;
 //     return "Hello world";
 // });
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobreNos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){ return 'login'; } )->name('site.login');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::prefix('/app')->group(function(){ 
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+    // Deve receber aqui, as Routes que deveram possuir esse prefixo
+    Route::get('/clientes',
+    function(){ return 'clientes'; })->name('app.clientes');
 
+    Route::get('/fornecedores',
+    function(){ return 'fornecedores'; })->name('app.fornecedores');
+
+    Route::get('/produtos',
+    function(){ return 'produtos'; })->name('app.produtos');
+});
+
+Route::get('/rota1', function(){
+    echo 'Rota1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+
+// Route::redirect('/rota2','/rota1');
 
 /* 
 Passando parametros pelas route
